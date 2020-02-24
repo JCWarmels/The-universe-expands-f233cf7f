4cdb4ddc421d64adcf028c9ae1da286d8c037602
JCWarmels <jesse.warmels@student.rocva.nl>
2020-02-24 10:14:54 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE sterrenstelsel;
+
+USE sterrenstelsel;
+
+create table planeten (naam varchar(10));
+
+INSERT INTO planeten (naam)
+VALUES  ('Zon'),
+		('Mercurius'),
+        ('Venus'),
+        ('Aarde'),
+        ('Mars')
+
+
+
+
+
+
+
+
+
